d8972b1ff815fb4c56f959ba5f43b0204f782c2c
abdullah <abdullahbinmasood02@gmail.com>
2026-05-12 09:24:32 +0500

    added error handling for invalid routes

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('hello from middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -23,5 +18,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `cant find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
